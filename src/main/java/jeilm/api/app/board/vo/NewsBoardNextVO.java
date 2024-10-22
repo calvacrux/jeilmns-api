76a027fac632f0b6d9fe070055debb522caee5da
parkbongil <bipark@gmail.com>
2024-10-22 13:35:08 +0900

    SQL 수정

--- src/main/java/jeilm/api/app/board/vo/NewsBoardNextVO.java
+++ src/main/java/jeilm/api/app/board/vo/NewsBoardNextVO.java
@@ -16,11 +16,9 @@ public class NewsBoardNextVO implements Serializable {
 	// 기본
 	@JsonIgnore
 	private String board_id;					// 게시판 아이디
-	@JsonIgnore
 	private String post_sn;						// 포스트 일련번호
 	private String post_cat;					// 포스트 카테고리
-	private String next_post_sn;	    		// 포스트 일련번호 - 다음
-	private String next_post_title;				// 포스트 제목 - 다음
+	private String post_title;					// 포스트 제목
 	
 	// 요청
 	@JsonProperty(access = Access.WRITE_ONLY)
--- src/main/java/jeilm/api/app/board/vo/NewsBoardPrevVO.java
+++ src/main/java/jeilm/api/app/board/vo/NewsBoardPrevVO.java
@@ -16,11 +16,9 @@ public class NewsBoardPrevVO implements Serializable {
 	// 기본
 	@JsonIgnore
 	private String board_id;					// 게시판 아이디
-	@JsonIgnore
 	private String post_sn;						// 포스트 일련번호
 	private String post_cat;					// 포스트 카테고리
-	private String prev_post_sn;	    		// 포스트 일련번호 - 이전
-	private String prev_post_title;				// 포스트 제목 - 이전
+	private String post_title;					// 포스트 제목
 	
 	// 요청
 	@JsonProperty(access = Access.WRITE_ONLY)
