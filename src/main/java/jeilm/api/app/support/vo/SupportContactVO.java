17c5048547d16d434fafbc21edf62a83a24baf14
parkbongil <bipark@gmail.com>
2024-10-29 10:22:52 +0900

    연락처 배경이미지 추가

--- src/main/java/jeilm/api/app/support/vo/SupportContactVO.java
+++ src/main/java/jeilm/api/app/support/vo/SupportContactVO.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import jeilm.api.cmm.constant.AppConstant;
 import lombok.Data;
 
 @Data
@@ -20,6 +21,22 @@ public class SupportContactVO implements Serializable {
 	private String contact_nm;				// 연락처 이름
 	private String contact_tel;				// 연락처 전화번호
 	private String contact_mail;			// 연락처 메일주소
+	
+	// 파일 - 첨부
+	@JsonIgnore
+	private String file_user_nm_main;			// 첨부 파일 이름
+	@JsonIgnore
+	private String file_path_main;				// 첨부 파일 경로
+		
+	// 파일 - URL
+	public String file_url_main;				// 메인 파일 URL
+	public String getFile_url_main() {
+		if (file_path_main == null) {
+			return null;
+		} else {
+			return AppConstant.storagePath.concat(file_path_main);
+		}
+	}
 	
 	// 요청
 	@JsonProperty(access = Access.WRITE_ONLY)
