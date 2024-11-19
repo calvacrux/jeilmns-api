342de0f4c467b0e8fb73c1616ebf12e107bb1d61
parkbongil <bipark@gmail.com>
2024-11-19 16:49:33 +0900

    제품 상세 수정

--- src/main/java/jeilm/api/app/product/vo/ProductRelationVO.java
+++ src/main/java/jeilm/api/app/product/vo/ProductRelationVO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jeilm.api.cmm.constant.AppConstant;
 import lombok.Data;
 
 @Data
@@ -20,5 +21,20 @@ public class ProductRelationVO implements Serializable {
 	private String category_nm;				// 카테고리 이름
 	private String info_sn;					// 정보 일련번호
 	private String info_title;				// 제품 타이틀
+	private String info_title_sub;				// 정보 타이틀 서브
+	
+	// 파일 - 썸네일 - 01
+	@JsonIgnore
+	private String file_path_thumb_01;			// 썸네일 파일 경로
+	
+	// 파일 - 썸네일 - 01
+	public String file_url_thumb_01;			// 썸네일 파일 URL
+	public String getFile_url_thumb_01() {
+		if (file_path_thumb_01 == null) {
+			return null;
+		} else {
+			return AppConstant.storagePath.concat(file_path_thumb_01);
+		}
+	}
 	
 }
