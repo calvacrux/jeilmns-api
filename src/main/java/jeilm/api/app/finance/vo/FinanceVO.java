e4a5a631ea19b16be9bb2f6b8048abd236c05b96
parkbongil <bipark@gmail.com>
2024-12-26 08:37:07 +0900

    재무상태에서 순투자자본 항목삭제

--- src/main/java/jeilm/api/app/finance/vo/FinanceVO.java
+++ src/main/java/jeilm/api/app/finance/vo/FinanceVO.java
@@ -14,11 +14,10 @@ public class FinanceVO implements Serializable {
 	private long asset_total;				// 자산총계
 	private long asset_current;				// 유동자산
 	private long asset_current_non;			// 비유동자산
-	private long debt_total;					// 부채총계
+	private long debt_total;				// 부채총계
 	private long debt_current;				// 유동부채
 	private long debt_current_non;			// 비유동부채
 	private long capital_total;				// 자본총계
-	private long capital_invest;				// 순투자자본
 	private long gain_loss_other_total;		// 기타포괄손익누계액
 	private long capital_other;				// 기타자본
 	
